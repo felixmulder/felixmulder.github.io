0c1c1c3ce659571e01ae9361c552eb8ac43e889d
Felix Mulder <felix.mulder@gmail.com>
2022-03-20 08:22:17 +0100

    Remove unused files

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Revision history for homepage
-
-## 0.1.0.0 -- 2019-09-30
-
-* First version. Released on a suspecting world.
--- old/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  hljs.initHighlightingOnLoad()
-
-  if ($("#markdown").length == 1) {
-      var editor = new EpicEditor({
-          container: "markdown"
-      }).load();
-  }
-})
